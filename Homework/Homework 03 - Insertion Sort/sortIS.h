10dbcc4af7952f1f9acb8aaf5e40b0d5acb6907c
Jim Moua <jimmoua97@live.com>
2018-11-29 18:37:10 -0600

    Implemented the insertion sort. Got it from the book.

--- Homework/Homework 03 - Insertion Sort/sortIS.h
+++ Homework/Homework 03 - Insertion Sort/sortIS.h
@@ -1,4 +1,33 @@
 #ifndef SORT_IS_H
 #define SORT_IS_H
+#include "UList.h"
+
+template<class T>
+void sort(UList<T>& list)
+{
+  for(size_t i = 1; i < list.size(); i++)
+  {
+    if(list.items[i] < list.items[i-1])
+    {
+      // The item we need to copy into a specific place.
+      T temp = list.items[i];
+
+      /* Get the location so we can modify it. We still want to keep
+         i for iterating throught the for-loop. */
+      int loc = i;
+
+      /* Here, shift all the data to the left since we are
+       * planning to insert a value to a location. */
+      do
+      {
+        list.items[loc] = list.items[loc - 1];
+        loc--;
+      }while(loc > 0 && list.items[loc-1] > temp);
+
+      // Copy the temp into the location
+      list.items[loc] = temp;
+    }
+  }
+}
 
 #endif
